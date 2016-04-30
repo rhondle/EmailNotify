682c60045638401117cc3c3be29e73a4313340ca
Marty Anstey <rhondle@users.noreply.github.com>
2016-04-29 18:13:02 -0700

    Fixed tabulation

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
 Plugin Name: EmailNotify
 Plugin URI: https://github.com/rhondle/WP_EmailNotify
 Description: Sends an email to a predefined list of recipients whenever a new post is published.
@@ -14,13 +14,13 @@ function emailnotify_queue_email($post) {
 	$tbl = $wpdb->prefix.'emailnotify_emails';						// this is the table that holds the message queue
 	if ($post->post_status!='publish') return;						// skip if not directly a published post
 	$wpdb->insert($tbl,
-			array(
-				'subject'=>'',
-				'title'=>$post->post_title,
-				'status'=>'queued',
-				'post_url'=>$post->guid,
-				'created'=>current_time('mysql')
-			)
+		array(
+			'subject'=>'',
+			'title'=>$post->post_title,
+			'status'=>'queued',
+			'post_url'=>$post->guid,
+			'created'=>current_time('mysql')
+		)
 	);
 }
 
